8879ccb5cc9405050a6ae909204ad8ecd0bf10e7
MAC <zhangfeng@zhangfengdeMacBook-Pro.local>
2024-06-20 18:28:20 +0800

    Diff end1

--- Diff/quicklyDiff.js
+++ Diff/quicklyDiff.js
@@ -348,8 +348,19 @@ function createRenderer(options) {
         let i = count - 1
         // for 循环使得i递减，
         for (i; i >= 0; i--) {
-          if (i !== seq[s]) {
+          if (source[i] === -1) {
+            const pos = i + newStart
+            const newVnode = newChildren[pos]
+            const nextPos = pos + 1
+            const anchor = nextPos < newChildren.length ? newChildren[nextPos].el : null
+            patch(null, newVnode, container, anchor)
+          } else if (i !== seq[s]) {
             // 如果节点的索引i不等于seq[s]的值，说明该节点需要移动
+            const pos = i + newStart
+            const newVnode = newChildren[pos]
+            const nextPos = pos + 1
+            const anchor = nextPos < newChildren.length ? newChildren[nextPos] : null
+            insert(newVnode.el, container, anchor)
           } else {
             // 当i===seq[s]时，说明该位置的节点不需要移动， 只需要让s指向下一个位置
             s--
